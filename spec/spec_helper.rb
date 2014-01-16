f8a945a0ff0281f7f9d01503757fe5c462389ffd
Jason Petersen <jason@citusdata.com>
2014-01-16 15:19:20 -0800

    Add SimpleCov
    Enable it during specs by setting the COVERAGE environment variable.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,10 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
+if ENV['COVERAGE']
+  require 'simplecov'
+  SimpleCov.start 'test_frameworks'
+end
+
 require 'data_expander'
 
 Dir['./spec/support/**/*.rb'].sort.each { |f| require f }
